a7b0db926d02e0734ecee6534d2841ad8bae0f0c
stepGT <stepGT@mail.ru>
2022-03-08 20:26:43 +1000

    Added and output data

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,3 +1,4 @@
+import { posts } from '../../data/data';
 import styles from './Content.module.css';
 
 const Content = () => {
@@ -5,33 +6,14 @@ const Content = () => {
     <>
       <h1>Simple Blog</h1>
       <div className="posts">
-        <div className="post">
-          <h2>Post 1</h2>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente,
-            fugiat harum. Voluptatibus beatae corrupti nulla, qui odit mollitia
-            doloremque rerum magni rem aut laborum, maiores officiis laboriosam
-            hic. Ratione, voluptas?
-          </p>
-        </div>
-        <div className="post">
-          <h2>Post 2</h2>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente,
-            fugiat harum. Voluptatibus beatae corrupti nulla, qui odit mollitia
-            doloremque rerum magni rem aut laborum, maiores officiis laboriosam
-            hic. Ratione, voluptas?
-          </p>
-        </div>
-        <div className="post">
-          <h2>Post 3</h2>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente,
-            fugiat harum. Voluptatibus beatae corrupti nulla, qui odit mollitia
-            doloremque rerum magni rem aut laborum, maiores officiis laboriosam
-            hic. Ratione, voluptas?
-          </p>
-        </div>
+        {posts.map((post) => {
+          return (
+            <div key={post.id} className="post">
+              <h2>{post.title}</h2>
+              <p>{post.description}</p>
+            </div>
+          );
+        })}
       </div>
       <div className={styles.count}>
         <button>Get amount of posts</button>
