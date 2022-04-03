d6337b1ca2fdb1e420cafa8ad4287dc31423bba3
stepGT <stepGT@mail.ru>
2022-04-04 00:06:23 +1000

    Fixed console warn

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import '@/App.css';
 import Header from './components/Header/Header';
 import Content from '@containers/Content';
