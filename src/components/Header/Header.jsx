b7412011076b6cef8eb5c41bc060fbf6cf906795
stepGT <stepGT@mail.ru>
2022-04-06 21:49:17 +1000

    Added NavLink. Is active class

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,25 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.module.css';
 
 const Header = () => {
   return (
     <header>
       <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/login'>Login</Link>
+        <NavLink style={({ isActive }) => {
+            return {
+              color: isActive ? '#d15700' : '',
+            };
+          }} to='/'>Home</NavLink>
+        <NavLink style={({ isActive }) => {
+            return {
+              color: isActive ? '#d15700' : '',
+            };
+          }} to='/about'>About</NavLink>
+        <NavLink style={({ isActive }) => {
+            return {
+              color: isActive ? '#d15700' : '',
+            };
+          }} to='/login'>Login</NavLink>
       </nav>
     </header>
   );
