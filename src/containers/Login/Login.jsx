2a94b2ba8fa5a581dc62374ca249fc9580762ad4
stepGT <stepGT@mail.ru>
2022-04-07 02:52:20 +1000

    Added on submit handler

--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -1,3 +1,4 @@
+import { useParams, useNavigate, useLocation } from 'react-router-dom';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
@@ -5,6 +6,15 @@ import styles from './Login.module.css';
 import { Typography } from '@mui/material';
 
 const Login = () => {
+  let navigate = useNavigate();
+  let location = useLocation();
+  let params = useParams();
+  const onClickHandler = e => {
+    e.preventDefault();
+    console.log(e.nativeEvent.target[0].value);
+    console.log(e.nativeEvent.target[2].value);
+    navigate('/' + location.search);
+  };
   return (
     <Stack
       component='form'
@@ -15,6 +25,7 @@ const Login = () => {
       noValidate
       autoComplete='off'
       className={styles.LoginForm}
+      onSubmit={onClickHandler}
     >
       <Typography variant='h4' gutterBottom component='div'>
         Authorization
@@ -28,9 +39,9 @@ const Login = () => {
       />
       <Button
         className={styles.LoginFormButton}
-        onClick={() => {}}
         size='large'
         variant='contained'
+        type='submit'
       >
         Enter
       </Button>
