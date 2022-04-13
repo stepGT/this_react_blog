05a667c9c159aa52f0a953e682b32650b7bb5f74
stepGT <stepGT@mail.ru>
2022-04-14 01:41:25 +1000

    Added from pathname to component NotFound

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { PublicRoute } from '@routes/PublicRoute';
 import { PrivateRoute } from '@routes/PrivateRoute';
 import useAxios from '@hooks/useAxios';
@@ -12,6 +12,7 @@ import Main from '@containers/Main';
 import NotFound from './components/NotFound';
 
 const App = () => {
+  const location = useLocation();
   const { data, loaded } = useAxios('/posts');
   const [isLogin, setIsLogin] = useState(localStorage.getItem('isLogin'));
   const [uname, setUname] = useState(localStorage.getItem('uname'));
@@ -40,7 +41,7 @@ const App = () => {
             }
           />
           <Route path='404' element={<NotFound />} />
-          <Route path='*' element={<Navigate to='404' />} />
+          <Route path='*' element={<Navigate state={{ from: location }} to='404' />} />
         </Routes>
       </main>
       <Footer year={year} />
